70a531e4fb345d0d503b37456ac4a85edb144b63
Berlin <Berline_lucien@yahoo.com>
2022-05-09 20:49:24 -0400

    created file for assignment 1

---
+++ Assignment 1/Arrays_Strings.py
@@ -0,0 +1,31 @@
+"""
+Part 2: Arrays & Strings (1 hr):
+isStringPermutation(...)
+Implement the function isStringPermutation() that takes two Strings as parameters and returns a Boolean denoting whether the first string is a permutation of the second string.
+
+Below are some examples:
+isStringPermutation(s1: “asdf”, s2: “fsda”) == true
+isStringPermutation(s1: “asdf”, s2: “fsa”) == false
+isStringPermutation(s1: “asdf”, s2: “fsax”) == false
+
+"""
+
+
+def isStringPermutation(s1: str, s2: str) -> bool:
+
+
+
+
+
+
+    """
+pairsThatEqualSum(...)
+Implement the function pairsThatEqualSum() that takes an array of integers and a target integer and returns an array of pairs (i.e., an array of tuples) where each pair contains two numbers from the input array and the sum of each pair equals the target integer. (Order of the output does not matter).
+
+Below are some examples:
+pairsThatEqualSum(inputArray: [1, 2, 3, 4, 5], targetSum: 5) == [(1, 4), (2, 3)]
+pairsThatEqualSum(inputArray: [1, 2, 3, 4, 5], targetSum: 1) == []
+pairsThatEqualSum(inputArray: [1, 2, 3, 4, 5], targetSum: 7) == [(2, 5), (3, 4)]
+
+
+    """
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Uber-Career-Prep-Homework-Berline-Luci
+
+Homework Assignment 1
+Arrays, Strings, Linked Lists, Stacks & Queues, and Recursion
